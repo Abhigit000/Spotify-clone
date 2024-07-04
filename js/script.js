ca3dc55106375e0028ef41bea01c58a4ab436e6b
Abhishek <abtiwari9@gmail.com>
2024-07-04 20:12:03 +0530

    done

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder){
     curFolder = folder;
-    let a = await fetch(`http://192.168.29.172:3000/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -63,7 +63,7 @@ const playMusic = (track)=>{
 }
 
 async function displayAlbums(){
-    let a = await fetch(`http://192.168.29.172:3000/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -75,7 +75,7 @@ async function displayAlbums(){
         if(e.href.includes("/songs")){
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`http://192.168.29.172:3000/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
             console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
